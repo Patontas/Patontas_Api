9ccd25e7a0ea3426120466330e9facba201dcb19
Patontas <patontasaccesorios@gmail.com>
2023-05-16 06:22:30 -0500

    Api V1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(
     cors({
-        oorigin: '*',
+        origin: '*',
     })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
